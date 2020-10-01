d9b159a2004bec2bf9a4208a07b88ea6843ddf58
Austin Lambert <atlambert11@gmail.com>
2020-10-01 09:41:24 -0500

    Added date and time to the header of the page.

--- logic.js
+++ logic.js
@@ -1,11 +1,20 @@
-// Variables
+// Variables for date
 var day = $("#day");
+var date = new Date();
 
-
+//Variables
 
 
 //Display date at top of page
+$(day).append(date);
+
+
+//Color code timeblocks
 
 
 
-//
+//Save textarea through button press
+
+
+
+//Save data in textarea through page refresh
